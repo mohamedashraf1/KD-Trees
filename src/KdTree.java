10aaf2b94c1ad30c5abefe602871f238cab4698a
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2020-08-13 08:33:40 +0200

    added test cases files and fixed nearest()

--- src/KdTree.java
+++ src/KdTree.java
@@ -3,28 +3,33 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdDraw;
 
+
 public class KdTree {
     private Node root;
     private Point2D champ;
+    private double champdist;
 
     private static class Node {
         Node left, right;
         Point2D point;
         int level;
         int size;
+        RectHV rect;
 
-        public Node(Node left, Node right, Point2D point, int level, int size) {
+        public Node(Node left, Node right, Point2D point, int level, int size, RectHV rect) {
             this.left = left;
             this.right = right;
             this.point = point;
             this.level = level;
             this.size = size;
+            this.rect = rect;
         }
     }
 
     // construct the Tree
     public KdTree() {
-        root = new Node(null, null, null, 0, 0);
+        root = new Node(null, null, null, 0, 0, new RectHV(0, 0, 1, 1));
+
     }
 
     // is the set empty?
@@ -37,19 +42,24 @@ public class KdTree {
         return root.size;
     }
 
-    private Node put(Node itr, Point2D p, double px, double py, int level) {
-        if (itr == null) return new Node(null, null, p, level + 1, 1);
+    private Node put(Node itr, Point2D p, double px, double py, int level, double xMin, double yMin, double xMax, double yMax) {
+        if (itr == null) return new Node(null, null, p, level + 1, 1, new RectHV(xMin, yMin, xMax, yMax));
         if (itr.level % 2 == 0) { // for the even level(vertical)
             if (px < itr.point.x()) {
-                itr.left = put(itr.left, p, px, py, itr.level);
+                xMax = itr.point.x();
+                itr.left = put(itr.left, p, px, py, itr.level, xMin, yMin, xMax, yMax);
             } else {
-                itr.right = put(itr.right, p, px, py, itr.level);
+                xMin = itr.point.x();
+                itr.right = put(itr.right, p, px, py, itr.level, xMin, yMin, xMax, yMax);
             }
         } else { // for the odd level(horizontal)
-            if (py < itr.point.y())
-                itr.left = put(itr.left, p, px, py, itr.level);
-            else
-                itr.right = put(itr.right, p, px, py, itr.level);
+            if (py < itr.point.y()) {
+                yMax = itr.point.y();
+                itr.left = put(itr.left, p, px, py, itr.level, xMin, yMin, xMax, yMax);
+            } else {
+                yMin = itr.point.y();
+                itr.right = put(itr.right, p, px, py, itr.level, xMin, yMin, xMax, yMax);
+            }
         }
         itr.size = itr.size + 1;
         return itr;
@@ -60,12 +70,12 @@ public class KdTree {
         if (p == null)
             throw new IllegalArgumentException();
         if (root.size == 0) {
-            root = new Node(null, null, p, 0, 1);
+            root = new Node(null, null, p, 0, 1, new RectHV(0, 0, 1, 1));
             return;
         }
         if (contains(p))
             return;
-        root = put(root, p, p.x(), p.y(), root.level);
+        root = put(root, p, p.x(), p.y(), root.level, 0, 0, 1, 1);
     }
 
     // does the Tree contain point p?
@@ -172,44 +182,47 @@ public class KdTree {
         return points;
     }
 
-    private void nearestSearch(Point2D query, double qx, double qy, Node itr, double champdist) {
+
+    private void nearestSearch(Point2D query, double qx, double qy, Node itr) {
         if (itr == null) return;
+        if (champdist < itr.rect.distanceSquaredTo(query)) {
+            return;
+        }
+
         if (query.distanceSquaredTo(itr.point) < champdist) {
             champ = itr.point;
-            champdist = query.distanceSquaredTo(champ);
+            champdist = itr.point.distanceSquaredTo(query);
         }
+
         if (itr.level % 2 == 0) {
             if (qx < itr.point.x()) {//on the left side
-                nearestSearch(query, qx, qy, itr.left, champdist);
-                if (!champ.equals(itr.point))
-                    nearestSearch(query, qx, qy, itr.right, champdist);
+                nearestSearch(query, qx, qy, itr.left);
+                nearestSearch(query, qx, qy, itr.right);
             } else {
-                nearestSearch(query, qx, qy, itr.right, champdist);
-                if (!champ.equals(itr.point))
-                    nearestSearch(query, qx, qy, itr.left, champdist);
+                nearestSearch(query, qx, qy, itr.right);
+                nearestSearch(query, qx, qy, itr.left);
             }
         } else {
             if (qy < itr.point.y()) {
-                nearestSearch(query, qx, qy, itr.left, champdist);
-                nearestSearch(query, qx, qy, itr.right, champdist);
+                nearestSearch(query, qx, qy, itr.left);
+                nearestSearch(query, qx, qy, itr.right);
             } else {
-                nearestSearch(query, qx, qy, itr.right, champdist);
-                nearestSearch(query, qx, qy, itr.left, champdist);
+                nearestSearch(query, qx, qy, itr.right);
+                nearestSearch(query, qx, qy, itr.left);
             }
         }
-
     }
-
 
     // a nearest neighbor in the Tree to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException();
-        champ = new Point2D(Integer.MAX_VALUE, Integer.MAX_VALUE);
-        nearestSearch(p, p.x(), p.y(), root, p.distanceSquaredTo(champ));
+        //champ = new Point2D(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        champ = root.point;
+        champdist = p.distanceSquaredTo(champ);
+        nearestSearch(p, p.x(), p.y(), root);
         return champ;
     }
-
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
@@ -219,17 +232,31 @@ public class KdTree {
         test.insert(new Point2D(0.2, 0.3));
         test.insert(new Point2D(0.4, 0.7));
         test.insert(new Point2D(0.9, 0.6));
-        RectHV rectHV = new RectHV(0.25, 0, 0.5, 0.75);
-        Stack<Point2D> stack = (Stack<Point2D>) test.range(rectHV);
-        System.out.println(test.isEmpty());
-        for (Point2D point2D : stack) {
-            System.out.println(point2D.toString());
-        }
+//        String filename = args[0];
+//        In in = new In(filename);
+//
+//        KdTree test = new KdTree();
+//        while (!in.isEmpty()) {
+//            double x = in.readDouble();
+//            double y = in.readDouble();
+//            Point2D p = new Point2D(x, y);
+//            test.insert(p);
+//
+//        }
+        //test.print(test.root);
 
-        Point2D query = new Point2D(0, 0);
+//        RectHV rectHV = new RectHV(0.25, 0, 0.5, 0.75);
+//        Stack<Point2D> stack = (Stack<Point2D>) test.range(rectHV);
+        System.out.println(test.isEmpty());
+//        for (Point2D point2D : stack) {
+//            System.out.println(point2D.toString());
+//        }
+
+        Point2D query = new Point2D(0.81, 0.30);
+        ///Point2D query2 = new Point2D(0.5, 0.5);
         System.out.println(test.nearest(query).toString());
-        System.out.println(test.contains(query));
-        System.out.println(test.size());
+
+
         test.draw();
 
     }
